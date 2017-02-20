2fea6814f616124cf5944e569a6c76cef4ac5fee
ramkumarramarajbb <ramkumar.ramaraj@bankbazaar.com>
2017-02-20 19:02:45 +0530

    "rc-slider" & "rc-tooltip" was added & slider was created

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import {TextField, IconTextField} from '../widgets/HTMLInputElements'
+
+import RangeSlider from '../widgets/RangeSliderNew.js'
+
 import ExperienceStyle from './Experience.scss';
 
 export default class Experience extends React.Component {
@@ -10,9 +13,15 @@ export default class Experience extends React.Component {
     
     render() {
         return (
+                <div>
             <div className={ExperienceStyle.container}>
                 <IconTextField/>
+
             </div>
-        )
+
+         <RangeSlider/>
+
+        </div>
+)
     }
 }
