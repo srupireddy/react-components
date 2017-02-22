f6f63da3cc5e9df1a9fa2ed9be59acd554bf33f9
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2017-02-22 08:58:22 +0530

    Changed Slide.js to Arrow Function syntax so that the methods are auto-bound.

--- src/containers/Slide.js
+++ src/containers/Slide.js
@@ -1,38 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import SlideManager from './SlideManager.js';
+import { addAttribute, loadSlide, previousSlide, nextSlide } from '../actions/actions.js';
 
 import SlideStyle from './Slide.scss';
 import ButtonStyle from '../widgets/Button.scss';
 import Sprite from '../widgets/Sprite.scss';
-import { connect } from 'react-redux';
-import { addAttribute, loadSlide, previousSlide, nextSlide } from '../actions/actions.js';
 
 class Slide extends React.Component {
-    constructor(props) {
-        super(props);
-        this.navigateToNextSlide = this.navigateToNextSlide.bind(this);
-        this.navigateToPreviousSlide = this.navigateToPreviousSlide.bind(this);
-        this.handleCompletionOfCurrSlideAction = this.handleCompletionOfCurrSlideAction.bind(this);
-        this.navigateToNextSlideIfCurrSlideValid = this.navigateToNextSlideIfCurrSlideValid.bind(this);
-    }
-
-    handleCompletionOfCurrSlideAction(data) {
+    handleCompletionOfCurrSlideAction = (data) => {
         this.props.dispatch(addAttribute(this.props.state.viewState.component.name, data));
         this.navigateToNextSlide(data);
     }
 
-    navigateToPreviousSlide() {
+    navigateToPreviousSlide = () => {
         var data = this.props.state[this.props.state.viewState.component.name];
         this.props.dispatch(previousSlide(this.props.state.viewState.component.name, data));
     }
 
-    navigateToNextSlide(data) {
+    navigateToNextSlide = (data) => {
         var data = this.props.state[this.props.state.viewState.component.name];
         this.props.dispatch(nextSlide(this.props.state.viewState.component.name, data));
     }
 
-    navigateToNextSlideIfCurrSlideValid() {
+    navigateToNextSlideIfCurrSlideValid = () => {
         if (!this.activeComponentInstance.isInValidState()) {
             console.log("Oopss... You have not chosen anything");
             return;
