b535f4fce0691b755dd4c7db76cd3cb51651c69e
ramkumarramarajbb <ramkumar.ramaraj@bankbazaar.com>
2017-03-06 15:46:56 +0530

    Purpose component is added

---
+++ src/components/Purpose.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BaseComponent from './BaseComponent';
+import {DecorateWithImageAndLabel} from '../widgets/Decorator';
+import PurposeStyle from './Purpose.scss';
+
+export default class Purpose extends BaseComponent {
+    static options = [
+        {label: 'Purchase or construct on property I have already chosen', imageStyle: PurposeStyle.iconpurpose1},
+        {label: 'Transfer my existing home loan', imageStyle: PurposeStyle.iconpurpose2},
+        {label: 'I haven’t yet identified the property I’d like to purchase', imageStyle: PurposeStyle.iconpurpose3}
+    ];
+    render() {
+        let radioItems = Purpose.options.map(
+            (option) => (
+                <DecorateWithImageAndLabel key={option.label} containerStyle="col-md-3 col-xs-3 col-sm-3 radio-col" imageStyle={option.imageStyle} label={option.label}>
+                    <input type="radio" value={option.label} name='purpose' onChange={this.handlePurposeSelection} checked={this.props.data === option.label}/>
+                </DecorateWithImageAndLabel>
+            )
+        );
+
+        return (
+            <div style={{...this.props.style, width: '900px'}}>
+                {radioItems}
+            </div>            
+        )
+    }
+
+    handlePurposeSelection = (event) => {
+        let value = event.target.value;
+        this.notifyCompletion(value);
+    }
+}
--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Gender from './Gender.js';
 import City from './City.js';
 import WorkExperience from './WorkExperience.js';
 import EmploymentType from './EmploymentType.js';
+import Purpose from './Purpose.js';
 import Employer from './Employer.js';
 
 import Calendar from './Calendar.js';
 import Income from './Income.js'
 
-export {Gender, City, EmploymentType, Employer, WorkExperience, Calendar, Income};
+export {Gender, City, EmploymentType, Employer, WorkExperience, Calendar, Income, Purpose};
+
