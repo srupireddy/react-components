37a9b306336f96cf0cf39bb921310576c924dbd1
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2017-03-09 13:01:08 +0530

    Fixed some merge issues in previous commit

--- src/components/others/CoApplicantRelationship.js
+++ src/components/others/CoApplicantRelationship.js
@@ -14,10 +14,10 @@ export default class CoApplicantRelationship extends BaseComponent {
     }
 
     render() {
-        let radioItems = CoAppRelationship.options.map(
+        let radioItems = CoApplicantRelationship.options.map(
             (option) => {
                 let label = titleCase(option);
-                let icon = CoApplicantRelationship['icon' + label];
+                let icon = CoApplicantRelationshipStyle['icon' + label];
                 return (
                     <DecorateWithImageAndLabel key={option} imageStyle={icon} label={label} containerStyle="col-md-3 col-xs-3 col-sm-3 radio-col">
                         <input type="radio" value={option} name='coAppRelationship' onChange={this.handleCoAppRelationshipSelection} checked={this.state.selectedRelationship === option}/>
