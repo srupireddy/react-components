b6d618008b9218eff7b01ec7b20044d57c6f848c
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2017-03-14 17:43:15 +0530

    Removed the unused method on hte ActionHandler interface.

--- src/components/ActionHandler.js
+++ src/components/ActionHandler.js
@@ -1,5 +1,4 @@
 export default class ActionHandler {
-    onChange = (modelKey, attributeName, value) => {}
     onCompletion = (modelKey, payload) => {}
     showError = (message) => {}
     clearError = () => {}
