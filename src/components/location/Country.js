b3a41019aa83b10a6de03ddec09efb6fd099d3b6
Venkatesh Jayaraman <venkatesh.j@bankbazaar.com>
2017-03-27 13:20:16 +0530

    Changes to ResidencyStatus and Country components.

--- src/components/location/Country.js
+++ src/components/location/Country.js
@@ -1,58 +1,18 @@
 import React from 'react';
 import Select from 'react-select';
 
-var options = [
-	{ value: 'Australia', label: 'Australia' },
-	{ value: 'Bahrain', label: 'Bahrain' },
-	{ value: 'Belgium', label: 'Belgium' },
-	{ value: 'Canada', label: 'Canada' },
-	{ value: 'China', label: 'China' },
-	{ value: 'Denmark', label: 'Denmark' },
-	{ value: 'France', label: 'France' },
-	{ value: 'Germany', label: 'Germany' },
-	{ value: 'Hong Kong', label: 'Hong Kong' },
-	{ value: 'Indonesia', label: 'Indonesia' },
-	{ value: 'Italy', label: 'Italy' },
-	{ value: 'Japan', label: 'Japan' },
-	{ value: 'Kuwait', label: 'Kuwait' },
-	{ value: 'Malaysia', label: 'Malaysia' },
-	{ value: 'Netherlands', label: 'Netherlands' },
-	{ value: 'New Zealand', label: 'New Zealand' },
-	{ value: 'Oman', label: 'Oman' },
-	{ value: 'Qatar', label: 'Qatar' },
-	{ value: 'Russia', label: 'Russia' },
-	{ value: 'Saudi Arabia', label: 'Saudi Arabia' },
-	{ value: 'Singapore', label: 'Singapore' },
-	{ value: 'Spain', label: 'Spain' },
-	{ value: 'Switzerland', label: 'Switzerland' },
-	{ value: 'UAE', label: 'UAE' },
-	{ value: 'UK', label: 'UK' },
-	{ value: 'USA', label: 'USA' },
-	{ value: 'Yemen', label: 'Yemen' },
-	{ value: 'Other', label: 'Other' }
-];
+export default class Country extends React.Component {
+	static options = [ 'Australia', 'Bahrain', 'Belgium', 'Canada', 'China', 'Denmark', 'France', 'Germany', 'Hong Kong', 'Indonesia', 'Italy', 'Japan', 'Kuwait', 'Malaysia', 'Netherlands', 'New Zealand', 'Oman', 'Qatar', 'Russia', 'Saudi Arabia', 'Singapore', 'Spain', 'Switzerland', 'UAE', 'UK', 'USA', 'Yemen', 'Other'];
 
-var CountryField = React.createClass({
-	displayName: 'CountryField',
-	getInitialState () {
-		return {
-			disabled: false,
-			searchable: this.props.searchable,
-			clearable: true
-		};
-	},
-	updateValue (newValue) {
-		this.setState({
-			selectValue: newValue
-		});
-	},
-	render () {
+	render() {
+		let optionsTransformed = Country.options.map(value => ({value: value, label: value}));
 		return (
 			<div className="selectContainer">
-				<Select ref="stateSelect" placeholder={'Select Residence Country'} options={options} simpleValue clearable={this.state.clearable} name="selected-state" disabled={this.state.disabled} value={this.state.selectValue} onChange={this.updateValue} />
+				<Select simpleValue
+						value={this.props.value} onChange={this.props.onChange} options={optionsTransformed} 
+						placeholder='Select Residence Country'
+				/>
 			</div>
 		);
 	}
-});
-
-module.exports = CountryField;
+}
--- src/components/location/ResidencyStatus.js
+++ src/components/location/ResidencyStatus.js
@@ -8,48 +8,63 @@ import ResidencyStatusStyle from './ResidencyStatus.scss';
 
 export default class ResidencyStatus extends BaseComponent {
     static options = [
-        {value: 'CONSTRUCTED', label: 'Non Resident Indian', imageStyle: ResidencyStatusStyle.iconNRI},
-        {value: 'UNDER_CONSTRUCTION', label: 'Holder of PIO Card', imageStyle: ResidencyStatusStyle.iconPIOCardHolder},
-        {value: 'PURCHASE_LAND_AND_CONSTRUCT', label: 'Parents are Indian citizens', imageStyle: ResidencyStatusStyle.iconIndianParents}
+        {value: 'NRI', label: 'Non Resident Indian', imageStyle: ResidencyStatusStyle.iconNRI},
+        {value: 'PIO', label: 'Holder of PIO Card', imageStyle: ResidencyStatusStyle.iconPIOCardHolder},
+        {value: 'INDIAN_PARENTS', label: 'Parents are Indian citizens', imageStyle: ResidencyStatusStyle.iconIndianParents}
     ];
 
     state = {
-        selectedType: this.props.value
+        type: this.props.value ? this.props.value.residencyStatus: '',
+        residentCountry: this.props.value ? this.props.value.residentCountry: ''
     }
 
     render() {
         let radioItems = ResidencyStatus.options.map(
             (option) => (
-            <DecorateWithImageAndLabel key={option.label} containerStyle="col-md-2 col-xs-3 col-sm-3 radio-col" imageStyle={option.imageStyle} label={option.label}>
-                    <input type="radio" value={option.value} name='residencyStatus' onChange={this.handleResidencyStatusSelection} checked={this.state.selectedType === option.value}/>
-            </DecorateWithImageAndLabel>
-        )
-    );
+                <DecorateWithImageAndLabel key={option.label} containerStyle="col-md-2 col-xs-3 col-sm-3 radio-col" imageStyle={option.imageStyle} label={option.label}>
+                    <input type="radio" value={option.value} name='residencyStatus' onChange={this.handleResidencyStatusSelection} checked={this.state.type === option.value}/>
+                </DecorateWithImageAndLabel>
+            )
+        );
 
-    return (
-        <div className= {ResidencyStatusStyle.container}>
-            {radioItems}
-            <div className="slideSecondHeader" style={{margin: "10px auto"}}>Residence Country</div>
-            <Country label="States"/>
-        </div>
-        )
+        return (
+            <div className= {ResidencyStatusStyle.container}>
+                {radioItems}
+                <div className="slideSecondHeader" style={{margin: "10px auto"}}>Residence Country</div>
+                <Country value={this.state.residentCountry} onChange={this.handleResidentCountrySelection}/>
+            </div>
+        );
     }
 
-handleResidencyStatusSelection = (event) => {
-    let value = event.target.value;
-    this.setState({selectedType: value}, () => {this.notifyCompletion()});
-}
-
-getData() {
-    return this.state.selectedType;
-}
-
-validate() {
-    if (this.state.selectedType) {
-        return true;
-    } else {
-        this.props.handler.showError("Uh-oh! Please pick any one option to proceed");
-        return false;
+    handleResidencyStatusSelection = (event) => {
+        let value = event.target.value;
+        this.setState({type: value}, () => {this.notifyCompletionIfAllPopulated()});
     }
-}
+
+    handleResidentCountrySelection = (value) => {
+        this.setState({residentCountry: value}, () => {this.notifyCompletionIfAllPopulated()});
+    }
+
+    notifyCompletionIfAllPopulated = () => {
+        if (this.isAllCollected()) {
+            this.notifyCompletion();
+        }
+    }
+
+    getData() {
+        return this.state;
+    }
+
+    isAllCollected() {
+        return this.state.type && this.state.residentCountry;
+    }
+
+    validate() {
+        if (isAllCollected()) {
+            return true;
+        } else {
+            this.props.handler.showError("Uh-oh! Please select an option to proceed");
+            return false;
+        }
+    }
 }
